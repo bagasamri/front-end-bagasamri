98d15179911bfc677e171005caee40d70d73cb34
bagasamri <bagas.amri7@gmail.com>
2019-12-02 11:32:30 +0000

    ini adalah hasi front-end user interface bagasamri

---
+++ index.php
@@ -0,0 +1,278 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <title>Company Profile</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"><strong>GOO WEB DEVELOPER </strong> </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+          <div class="collapse navbar-collapse ml-5" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link ml-3" href="#">HOME <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link ml-3" href="#">ABOUT US</a>
+              <a class="nav-item nav-link ml-3" href="#">OUR SERVICE</a>
+              <a class="nav-item nav-link ml-3" href="#">PORTFOLIO</a>
+              <a class="nav-item nav-link ml-3" href="#">CONTACT US</a>
+            </div>
+          </div>
+      </div>
+   </nav>
+
+   <div class="jumbotron">
+    <h1 class="display-4">Buat website Anda Sekarang!!</h1>
+      <hr class="my-4">
+    <p class="lead">Jasa pembuatan  dengan harga terjangkau dan proses cepat</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Buat sekarang</a>
+  </div>
+  
+        <div class="container">
+          <div class="row">
+          <div class="card text-center">
+              <div class="card-header h3">
+                PELAJAR <br>
+                <span class="display-4">RP.99.000</span>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush mb-4">
+                  <li class="list-group-item">Kapasitas Halaman :20 Halaman</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Kuota Upload 300 MB</li>
+                  <li class="list-group-item">Bandwidth : Unlimited</li>
+                  <li class="list-group-item">Akun Email : 3</li>
+                </ul>
+              <a href="#" class="btn btn-primary rounded-pill">Beli sekarang</a>
+              </div>
+            </div>
+
+            <div class="card text-center">
+              <div class="card-header h3">
+                PELAJAR <br>
+                <span class="display-4">RP.99.000</span>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush mb-4">
+                  <li class="list-group-item">Kapasitas Halaman :20 Halaman</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Kuota Upload 300 MB</li>
+                  <li class="list-group-item">Bandwidth : Unlimited</li>
+                  <li class="list-group-item">Akun Email : 3</li>
+                </ul>
+              <a href="#" class="btn btn-primary rounded-pill">Beli sekarang</a>
+              </div>
+            </div>
+            <div class="card text-center">
+              <div class="card-header h3">
+                PELAJAR <br>
+                <span class="display-4">RP.99.000</span>
+              </div>
+              <div class="card-body">
+                <ul class="list-group list-group-flush mb-4">
+                  <li class="list-group-item">Kapasitas Halaman :20 Halaman</li>
+                  <li class="list-group-item">Gratis Domain</li>
+                  <li class="list-group-item">Kuota Upload 300 MB</li>
+                  <li class="list-group-item">Bandwidth : Unlimited</li>
+                  <li class="list-group-item">Akun Email : 3</li>
+                </ul>
+              <a href="#" class="btn btn-primary rounded-pill">Beli sekarang</a>
+              </div>
+            </div>
+        </div>
+      </div>
+
+      <h3 class="mt-5 text-center">ABOUT US</h3>
+      <hr>
+      <div class="bg-light">
+        <div class="container text-justify p-4">
+        <div class="row">
+          <div class="col-md-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div class="col-md-6">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          
+        </div>
+        </div>
+
+      </div>
+
+       <h3 class="mt-5 text-center">AUR SERVICE</h3>
+      <hr>
+      <div class="container">
+        <div class="row text-center mt-5">
+          <div class="col-md-4">
+            <i class="fas fa-laptop-code h1"></i>
+            <h3>Expert coder</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div class="col-md-4">
+            <i class="fas fa-crop-alt h1"></i>
+            <h3>Graphic Desingner</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div class="col-md-4">
+              <i class="fas fa-camera-retro h1"></i>
+            <h3>Sosial Marketer</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+       </div>
+
+       <div class="row mt-5 text-center">
+          <div class="col-md-4">
+            <i class="fas fa-chart-bar h1"></i>
+            <h3>SEO Exprart</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div class="col-md-4">
+            <i class="fas fa-clock h1"></i>
+            <h3>24/7 Support</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+          <div class="col-md-4">
+              <i class="fas fa-chart-line h1"></i>
+            <h3>System Analyst</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+       </div> 
+      </div>
+
+       <h3 class="mt-5 text-center">PORTFOLIO</h3>
+      <hr>
+        <div class="container">
+        <div class="row text-center mt-2">
+          <div class="col-md-4">
+            <a href=""><img src="img/poro1.png" width="300px;"></a>
+          </div>
+          <div class="col-md-4">
+           <a href=""><img src="img/poro1.png"width="300px;" ></a>
+          </div>
+          <div class="col-md-4">
+            <a href=""><img src="img/poro1.png"width="300px;" ></a>
+          </div>
+          </div>
+       <div class="row text-center mt-2">
+          <div class="col-md-4">
+            <a href=""><img src="img/poro1.png" width="300px;"></a>
+          </div>
+          <div class="col-md-4">
+           <a href=""><img src="img/poro1.png"width="300px;" ></a>
+          </div>
+          <div class="col-md-4">
+            <a href=""><img src="img/poro1.png" width="300px;"></a>
+          </div>
+          </div>
+       </div>
+      </div>
+
+       <h3 class="mt-5 mb-3 text-center">CONTACT US</h3>
+      <hr>
+      <div class="container">
+        <div class="row">
+        <div class="col-md-7">
+          <div class="form-group">
+            <label><strong>Nama Lengkap</strong></label>
+            <input type="text" name="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label><strong>Email</strong></label>
+            <input type="email" name="" class="form-control">
+          </div>
+          <div class="form-group">
+            <label><strong>Pesan</strong></label>
+            <textarea type="text" class="form-control" row="5"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Kirim</button>
+        </div>
+        <div class="col-md-3">
+          <img src="img/customer.jpg" width="350px">
+        </div>
+      </div>
+      </div>
+
+<footer class="mt-5 bg-dark">
+  <div class="container">
+    <div class="row text-white p-4 text-justify">
+      <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+         <div class="col-md-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+         <div class="col-md-4">
+          <strong>Langganan Newsletter </strong>
+          <input type="text" name="" class="form-control rounded-pill mt-3" placeholder="Masukan Email Anda">
+        <button class="btn btn-outline-primary rounded-circle mr-3 mt-3">
+          <i class="fab fa-instagram"> </i>
+        </button>
+          <button class="btn btn-outline-primary rounded-circle mr-3 mt-3">
+          <i class="fab fa-facebook-square"> </i>
+        </button>
+          <button class="btn btn-outline-primary rounded-circle mr-3 mt-3">
+          <i class="fab fa-twitter-square"> </i>
+        </button>
+          <button class="btn btn-outline-primary rounded-circle mr-3 mt-3">
+          <i class="fab fa-youtube"> </i>
+        </button>
+          <button class="btn btn-outline-primary rounded-circle mr-3 mt-3">
+          <i class="fas fa-envelope"> </i>
+        </button>
+
+
+
+
+        </div>
+      
+    </div>
+    
+  </div>
+</footer>
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
